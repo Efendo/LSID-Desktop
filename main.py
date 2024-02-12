ff21225b202163ffc74253648874a92a48698cd8
Efe Mustafa Öner <efe@debian.fritz.box>
2024-02-12 18:15:29 +0100

    added view file function

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from whiptail import Whiptail
 import os
 import subprocess
 import getpass
+from pathlib import Path
 
 # Get current username
 user = getpass.getuser()  
+
+# Get home directory
+home = str(Path.home())
 
 # Create main Whiptail window 
 window = Whiptail(title="LSID Desktop", backtitle=f"USER: {user}, OS based on: {os.name}")
@@ -16,23 +20,27 @@ window.msgbox("Welcome to LSID Desktop")
 # Define menu function to show menu and get selection
 def menu():
     window = Whiptail(title="LSID Desktop: Menu", backtitle=f"USER: {user}, OS based on: {os.name}")
-    me=window.menu("What do you want to do today?", ["Do Math", "Play a Game", "End LSID Desktop"])[0]
+    me=window.menu("What do you want to do today?", ["View a file", "End LSID Desktop"])[0]
     return me
+
+def ViewFile():
+    file=window.inputbox("Enter some text:", f"{home}/")[0]
+    if os.path.isfile(file):
+        with open(file) as f:
+            window.msgbox(f.read())
+    else:
+        window.msgbox("That's a directory!")
+        ViewFile()
 
 # Main loop
 while True:
     # Show menu and get selection
     me = menu()
-    
-    # Check if "End LSID Desktop" was not selected
-    if me != "End LSID Desktop":
-        # Show message box with selection
-        window.msgbox(me)  
-        
-    else:
-        # Show ending message
-        window.msgbox("Ending LSID Desktop...")
-    
+
+    # DISPLAY FILE
+    if me == "View a file":
+        ViewFile()
+
     # Check if end was selected    
     if me == "End LSID Desktop":
         # Exit program
